fce319e2bed855fb37701e6bbc9c914a8f759050
will <flcoder@gmail.com>
2022-07-30 04:04:38 +0000

    fixed js error

--- src/lib/sections/McAnimation.svelte
+++ src/lib/sections/McAnimation.svelte
@@ -429,21 +429,23 @@
                     pointer.x = touches[i].pageX;
                     pointer.y = touches[i].pageY;
                 }
-            },
-            false,
+            }, {passive:true}
         );
 
         function m(t) {
-            for (var e, n = document.getElementById(t), i = n.innerHTML.replace("&amp;", "&").split(""), a = "", o = 0, s = i.length; s > o; o++) {
-                e = i[o].replace("&", "&amp");
-                a += e.trim() ? '<span class="letter-' + o + '">' + e + "</span>" : "&nbsp;";
+            var n = document.getElementById(t)
+            if(n) {
+              for (var e, n = document.getElementById(t), i = n.innerHTML.replace("&amp;", "&").split(""), a = "", o = 0, s = i.length; s > o; o++) {
+                  e = i[o].replace("&", "&amp");
+                  a += e.trim() ? '<span class="letter-' + o + '">' + e + "</span>" : "&nbsp;";
+              }
+
+              n.innerHTML = a;
+
+              setTimeout(function () {
+                  n.className = "transition-in";
+              }, 500 * Math.random() + 500);
             }
-
-            n.innerHTML = a;
-
-            setTimeout(function () {
-                n.className = "transition-in";
-            }, 500 * Math.random() + 500);
         }
 
         window.onload = function () {
